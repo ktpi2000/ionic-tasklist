1cf4a1b376c0addb940dbb9e3ed868e9533b64db
ktpi2000 <katapi2000@gmail.com>
2020-01-22 22:21:19 +0900

    アクションシートの表示

--- src/app/task-list/task-list.page.ts
+++ src/app/task-list/task-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-task-list',
@@ -10,7 +11,9 @@ export class TaskListPage implements OnInit {
     { name: 'タスク1' },
     { name: 'タスク2' },
   ];
-  constructor() { }
+  constructor(
+    public actionSheetController: ActionSheetController,
+  ) { }
 
   ngOnInit() {
   }
@@ -19,5 +22,34 @@ export class TaskListPage implements OnInit {
     if ('tasks' in localStorage) {
       this.tasks = JSON.parse(localStorage.tasks);
     }
+  }
+
+  async changeTask(index: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'タスク変更',
+      buttons: [
+        {
+          text: '削除',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        }, {
+          text: '変更',
+          icon: 'create',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        }, {
+          text: '閉じる',
+          icon: 'close',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+      ]
+    });
+    actionSheet.present();
   }
 }
